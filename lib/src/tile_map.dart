22b8293f83f412e0463a0cd8cf843ee8f79ad36c
Arron Washington <arron@theradicaledwards.com>
2014-02-02 21:16:24 -0500

    Better docs for getTileByGID.

--- lib/src/tile_map.dart
+++ lib/src/tile_map.dart
@@ -7,13 +7,16 @@ class TileMap {
   List<Tileset> tilesets = new List<Tileset>();
   List<Layer> layers = new List<Layer>();
 
-  // Retrieve a tile based on its GID
-  // GID is 1-based
-  // From offical documentation:
-  // In order to find out from which tileset the tile is you need to find the
-  // tileset with the highest firstgid that is still lower or equal than the gid.
-  // The tilesets are always stored with increasing firstgids.
-  // A GID of 0 is always an "empty" tile
+  /**
+   * Retrieve a tile based on its GID
+   *
+   * GID is 1-based
+   * From offical documentation:
+   * In order to find out from which tileset the tile is you need to find the
+   * tileset with the highest firstgid that is still lower or equal than the gid.
+   * The tilesets are always stored with increasing firstgids.
+   * A GID of 0 is always an "empty" tile
+   */
   Tile getTileByGID(int gid) {
     if (gid == 0) { return new Tile.emptyTile(); }
     var ts = tilesets.lastWhere((tileset) => tileset.firstgid <= gid);

6f522b02cfa31dd6af700599aee6e980791b7427
feroult <feroult@gmail.com>
2018-12-31 20:43:48 -0200

    layer visible

--- lib/src/layer.dart
+++ lib/src/layer.dart
@@ -2,39 +2,47 @@ part of tmx;
 
 class Layer {
   static const int FLIPPED_HORIZONTALLY_FLAG = 0x80000000;
-  static const int FLIPPED_VERTICALLY_FLAG   = 0x40000000;
-  static const int FLIPPED_DIAGONALLY_FLAG   = 0x20000000;
+  static const int FLIPPED_VERTICALLY_FLAG = 0x40000000;
+  static const int FLIPPED_DIAGONALLY_FLAG = 0x20000000;
 
   String name;
   int width;
   int height;
+  bool visible;
 
   TileMap map;
   List<List<int>> tileMatrix;
 
   List<Tile> _tiles;
   List<Tile> get tiles {
-    if (_tiles == null) { _recalculateTiles(); }
+    if (_tiles == null) {
+      _recalculateTiles();
+    }
     return _tiles;
-
   }
 
   Layer(this.name, this.width, this.height);
 
   Layer.fromXML(XmlElement element) {
-    if (element == null) { throw 'arg "element" cannot be null'; }
-
+    if (element == null) {
+      throw 'arg "element" cannot be null';
+    }
 
     NodeDSL.on(element, (dsl) {
-      name   = dsl.strOr('name', name);
-      width  = dsl.intOr('width', width);
+      name = dsl.strOr('name', name);
+      width = dsl.intOr('width', width);
       height = dsl.intOr('height', height);
+      visible = dsl.boolOr('visible', true);
     });
 
-    var dataElement = element.children.firstWhere((node) => node is XmlElement && node.name.local == 'data', orElse: () => null);
+    var dataElement = element.children.firstWhere(
+        (node) => node is XmlElement && node.name.local == 'data',
+        orElse: () => null);
     if (dataElement is XmlElement) {
-      var decoder = TileMapParser._getDecoder(dataElement.getAttribute('encoding'));
-      var decompressor = TileMapParser._getDecompressor(dataElement.getAttribute('compression'));
+      var decoder =
+          TileMapParser._getDecoder(dataElement.getAttribute('encoding'));
+      var decompressor = TileMapParser._getDecompressor(
+          dataElement.getAttribute('compression'));
 
       var decodedString = decoder(dataElement.text);
       var inflatedString = decompressor?.call(decodedString) ?? decodedString;
@@ -48,11 +56,9 @@ class Layer {
     tileMatrix = new List<List<int>>(height);
 
     var tileIndex = 0;
-    for(var y = 0; y < height; ++y) {
+    for (var y = 0; y < height; ++y) {
       tileMatrix[y] = new List<int>(width);
-      for(var x = 0; x < width; ++x) {
-
-
+      for (var x = 0; x < width; ++x) {
         var globalTileId = bytes[tileIndex] |
             bytes[tileIndex + 1] << 8 |
             bytes[tileIndex + 2] << 16 |
@@ -79,12 +85,12 @@ class Layer {
   _recalculateTiles() {
     var x, y = 0;
     _tiles = new List<Tile>();
-    tileMatrix.forEach( (List<int> row) {
+    tileMatrix.forEach((List<int> row) {
       x = 0;
       row.forEach((int tileId) {
         var tile = map.getTileByGID(tileId)
-            ..x = x
-            ..y = y;
+          ..x = x
+          ..y = y;
 
         // compute imageX,imageY
 

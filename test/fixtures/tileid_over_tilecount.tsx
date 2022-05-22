b991bfc9413cec38d7511546e15d34fdf4d18bc3
Nathan <tekkbot@gmail.com>
2022-05-22 08:10:54 -0700

    fix: First tile in an external tileset was always empty (#46)
    * fixed a bug where the first tile in an external tileset was always empty.
    
    * made bad tile tileid = -1 and fixed tests

---
+++ test/fixtures/tileid_over_tilecount.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.90" name="external" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="level1.png" width="272" height="128"/>
+ <tile id="0" type="first_tile"/>
+ <tile id="136">
+  <properties>
+   <property name="type" value="expands_tilecount"/>
+  </properties>
+ </tile>
+</tileset>

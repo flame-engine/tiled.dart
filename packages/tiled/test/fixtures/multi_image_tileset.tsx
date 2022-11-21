a0ab0f14228b47f82fcbdb6ad943cee78348263b
Ryuzaki <ufrshubham@gmail.com>
2022-11-21 23:03:28 +0530

    Add tests

---
+++ packages/tiled/test/fixtures/multi_image_tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="multi_image_tileset" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" x="64" y="96" width="32" height="32">
+  <image width="256" height="256" source="floortileset.png"/>
+ </tile>
+ <tile id="1" x="0" y="0" width="20" height="20">
+  <image width="160" height="20" source="coins.png"/>
+ </tile>
+</tileset>
--- packages/tiled/test/tile_test.dart
+++ packages/tiled/test/tile_test.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:test/test.dart';
 import 'package:tiled/tiled.dart';
+import 'package:xml/xml.dart';
 
 void main() {
   group('Tile.emptyTile()', () {
@@ -25,4 +29,53 @@ void main() {
     final tile = Tile(localId: -1);
     expect(tile.properties, isA<CustomProperties>());
   });
+
+  group(
+    'Tile.imageRect',
+    () {
+      late Tileset tileset1;
+      late Tileset tileset2;
+
+      setUp(() {
+        final f1 = File('./test/fixtures/multi_image_tileset.tsx')
+            .readAsString()
+            .then((xml) {
+          final tilesetXml = XmlDocument.parse(xml).rootElement;
+          tileset1 = Tileset.parse(XmlParser(tilesetXml));
+        });
+
+        final f2 = File('./test/fixtures/tileid_over_tilecount.tsx')
+            .readAsString()
+            .then((xml) {
+          final tilesetXml = XmlDocument.parse(xml).rootElement;
+          tileset2 = Tileset.parse(XmlParser(tilesetXml));
+        });
+
+        return Future.wait([f1, f2]);
+      });
+
+      test('with image collections', () {
+        final tile1 = tileset1.tiles.firstWhere((t) => t.localId == 0);
+        final tile2 = tileset1.tiles.firstWhere((t) => t.localId == 1);
+
+        expect(tile1.imageRect, const Rect.fromLTWH(64, 96, 32, 32));
+        expect(tile2.imageRect, const Rect.fromLTWH(0, 0, 20, 20));
+      });
+
+      test(
+        'with single image',
+        () {
+          final tile1 = tileset2.tiles.firstWhere((t) => t.localId == 58);
+          final tile2 = tileset2.tiles.firstWhere((t) => t.localId == 106);
+          final tile3 = tileset2.tiles.firstWhere((t) => t.localId == 129);
+          final tile4 = tileset2.tiles.firstWhere((t) => t.localId == 11);
+
+          expect(tile1.imageRect, const Rect.fromLTWH(112, 48, 16, 16));
+          expect(tile2.imageRect, const Rect.fromLTWH(64, 96, 16, 16));
+          expect(tile3.imageRect, const Rect.fromLTWH(160, 112, 16, 16));
+          expect(tile4.imageRect, const Rect.fromLTWH(176, 0, 16, 16));
+        },
+      );
+    },
+  );
 }
